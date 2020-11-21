12184b9b55accc85bb42780b9845e6783f197bfa
AI-Pranto <pranto.roza.53@gmail.com>
2020-11-21 10:53:17 +0600

    remove g++

--- tools/ci/travis-install-dagmc.sh
+++ tools/ci/travis-install-dagmc.sh
@@ -1,11 +1,8 @@
-
 #!/bin/bash
 set -ex
 
 # MOAB Variables
-MOAB_BRANCH='Version5.1.0'
-MOAB_REPO='https://bitbucket.org/fathomteam/moab/'
-MOAB_INSTALL_DIR=$HOME/MOAB/
+MOAB_INSTALL_DIR=$HOME/miniconda/envs/openmc
 
 # DAGMC Variables
 DAGMC_BRANCH='develop'
@@ -14,21 +11,12 @@ DAGMC_INSTALL_DIR=$HOME/DAGMC/
 
 CURRENT_DIR=$(pwd)
 
-# MOAB Install
-cd $HOME
-mkdir MOAB && cd MOAB
-git clone -b $MOAB_BRANCH $MOAB_REPO
-mkdir build && cd build
-cmake ../moab -DENABLE_HDF5=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$MOAB_INSTALL_DIR -DENABLE_BLASLAPACK=OFF
-make -j && make -j install
-rm -rf $HOME/MOAB/moab $HOME/MOAB/build
-
 # DAGMC Install
 cd $HOME
 mkdir DAGMC && cd DAGMC
 git clone -b $DAGMC_BRANCH $DAGMC_REPO
 mkdir build && cd build
-cmake ../dagmc -DBUILD_TALLY=ON -DCMAKE_INSTALL_PREFIX=$DAGMC_INSTALL_DIR -DBUILD_STATIC_LIBS=OFF -DMOAB_DIR=$MOAB_INSTALL_DIR
+cmake ../dagmc -DBUILD_TALLY=ON -DCMAKE_INSTALL_PREFIX=$DAGMC_INSTALL_DIR -DMOAB_DIR=$MOAB_INSTALL_DIR
 make -j install
 rm -rf $HOME/DAGMC/dagmc $HOME/DAGMC/build
 

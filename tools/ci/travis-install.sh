1956bb3f3eab8575e7465dbb6a50cdc337533aab
AI-Pranto <pranto.roza.53@gmail.com>
2020-11-21 16:16:03 +0600

    more explicit g++

--- tools/ci/travis-install.sh
+++ tools/ci/travis-install.sh
@@ -4,8 +4,8 @@ set -ex
 # Install conda packages
 conda install eigen fortran-compiler pthread-stubs
 conda install gxx_linux-64
-ln -s $HOME/miniconda/bin/x86_64-conda-cos6-linux-gnu-g++ $HOME/miniconda/bin/g++
-ln -s $HOME/miniconda/bin/x86_64-conda-cos6-linux-gnu-gcc $HOME/miniconda/bin/gcc
+ln -s $HOME/miniconda/envs/openmc/bin/x86_64-conda-cos6-linux-gnu-g++ $HOME/miniconda/envs/openmc/bin/g++
+ln -s $HOME/miniconda/envs/openmc/bin/x86_64-conda-cos6-linux-gnu-gcc $HOME/miniconda/envs/openmc/bin/gcc
 which gcc
 which g++
 
